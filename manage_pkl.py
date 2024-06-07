147d705515f35fb00cfec447d0daedb420de41ad
katao <k.katasan@gmail.com>
2024-06-07 10:51:11 +0900

    角巻わため曲を登録。1626(+4)

--- manage_pkl.py
+++ manage_pkl.py
@@ -16,6 +16,10 @@ if __name__ == '__main__':
     a = load()
 
     ##a['titles']['title'] = ['title', 'artist', 'bpm', 6,12,15,None]
-    a['titles'][''] = ['', '', '170', 6,12,15,18]
+    a['titles']['エクシード仮面ちゃんのちょっと一線をえくしーどしたEXCEED講座'] = ['エクシード仮面ちゃんのちょっと一線をえくしーどしたEXCEED講座', '', '130', None,None,None,1]
+    a['titles']['愛昧ショコラーテ'] = ['愛昧ショコラーテ', '角巻わため', '135', 2,9,13,16]
+    a['titles']['曇天羊'] = ['曇天羊', '角巻わため', '135', 3,10,14,17]
+    a['titles']['My song'] = ['My song', '角巻わため', '188', 5,12,15,18]
+    a['titles']['What an amazing swing'] = ['What an amazing swing', '角巻わため', '125', 4,11,15,17]
 
     save(a)
